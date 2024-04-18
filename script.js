006ccc77f999f49119cbb88813c11ff55765e14d
Dj29563 <164483614+Dj29563@users.noreply.github.com>
2024-04-19 03:23:07 +0700

    rework script.js

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-function autoExpand(element) {
-    element.style.height = '30px';
-    element.style.height = element.scrollHeight + 'px';
+function showQuestion(question) {
+    if (question === 1) {
+        document.getElementById('question1').style.display = 'block';
+        document.getElementById('question2').style.display = 'none';
+        document.getElementById('question3').style.display = 'none';
+        document.getElementById('tab1').classList.add('active');
+        document.getElementById('tab2').classList.remove('active');
+    } else if (question === 2) {
+        document.getElementById('question1').style.display = 'none';
+        document.getElementById('question2').style.display = 'block';
+        document.getElementById('question3').style.display = 'none';
+        document.getElementById('tab1').classList.remove('active');
+        document.getElementById('tab2').classList.add('active');
+    }
 }
